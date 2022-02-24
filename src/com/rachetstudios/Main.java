f9d778c535b16f85f8259900d7d752429e1ae750
beyarkay <boydrkane@gmail.com>
2022-02-24 17:30:02 +0200

    Add method to get all input files

--- src/com/rachetstudios/Main.java
+++ src/com/rachetstudios/Main.java
@@ -1,9 +1,24 @@
 package com.rachetstudios;
 
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello world");
+    }
+
+
+    /**
+     * https://www.baeldung.com/java-list-directory-files
+     */
+    public static List<String> getAllInputFiles() {
+        return Stream.of(Objects.requireNonNull(new File("examples_in").listFiles()))
+                .filter(file -> !file.isDirectory())
+                .map(File::getName)
+                .collect(Collectors.toList());
     }
 }
