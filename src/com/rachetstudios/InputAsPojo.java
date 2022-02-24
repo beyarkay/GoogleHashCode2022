bb76a16187a431c192e4effe689e50511340f738
beyarkay <boydrkane@gmail.com>
2022-02-24 18:36:38 +0200

    Improve InputAsPojo and update temp input files

--- src/com/rachetstudios/InputAsPojo.java
+++ src/com/rachetstudios/InputAsPojo.java
@@ -12,13 +12,25 @@ public class InputAsPojo {
     // Represents the very first line, which usually just has ints
     ArrayList<Integer> first;
     // Represents the remainder of the lines which usually represent a bunch of custom objects. @see LineItem
+    // TODO rename rest to be more semantically appropriate (ie 'Cars' or something)
     ArrayList<LineItem> rest;
+    // Sometimes there are actually two arrays of items described. In this case, last contains those items
+    // TODO rename rest2 to be more semantically appropriate (ie 'People' or something)
+    ArrayList<OtherLineItem> otherRest;
+
 
     /**
+     * Convert a path to a Plain Ol' Java Object.
+     *
+     * It automatically detects how many tokens are on the first line, and adds them as Integers to
+     * `this.first`.
+     *
+     * It also will go through the rest of the file and add each remaining line as a LineItem object
+     * to the arraylist `this.rest`.
+     *
      * https://www.geeksforgeeks.org/fast-io-in-java-in-competitive-programming/
      */
     public InputAsPojo(String path) {
-
         try {
             System.out.println("Parsing file: " + path + "...");
             BufferedReader br = new BufferedReader(new FileReader(path));
@@ -30,16 +42,17 @@ public class InputAsPojo {
             }
 
             // TODO we're assuming here that the very first token on the first line contains the total number of line items
-            int numberOfInputLines = first.get(0);
-            // And now go through the rest of the file, reading numberOfInputLines and saving them to some dummy variable
-            while (numberOfInputLines-- > 0) {
+            int numberOfLineItems = first.get(0);
+
+            // And now go through the rest of the file, reading numberOfLineItems and saving them to some dummy variable
+            while (numberOfLineItems-- > 0) {
                 StringTokenizer stRest = new StringTokenizer(br.readLine());
                 // TODO remove this assertion once we know how many tokens there are per line
                 assert stRest.countTokens() == 4;
 
                 // Create a lineItem object to represent whatever is on the current line
                 LineItem lineItem = new LineItem(
-                        // TODO Right now all these classes are abitrary. Rename and refactor them once we know what they're called
+                        // TODO Right now all these classes are arbitrary. Rename and refactor them once we know what they're called
                         Integer.parseInt(stRest.nextToken()),
                         (String) (stRest.nextToken()),
                         FloatingDecimal.parseFloat(stRest.nextToken()),
@@ -47,6 +60,25 @@ public class InputAsPojo {
                 );
                 rest.add(lineItem);
             }
+
+            // TODO this is only important if there are more than one set of objects (ie Libraries and Books, or Intersections and People)
+//            int numberOfSecondaryLineItems = -1; // first.get(2);
+//            while (numberOfSecondaryLineItems-- > 0) {
+//                StringTokenizer stRest = new StringTokenizer(br.readLine());
+//                // TODO remove this assertion once we know how many tokens there are per line
+//                assert stRest.countTokens() == 4;
+//
+//                // Create a lineItem object to represent whatever is on the current line
+//                OtherLineItem otherLineItem = new OtherLineItem(
+//                        // TODO Right now all these classes are arbitrary. Rename and refactor them once we know what they're called
+//                        Integer.parseInt(stRest.nextToken()),
+//                        (String) (stRest.nextToken()),
+//                        FloatingDecimal.parseFloat(stRest.nextToken()),
+//                        Double.parseDouble(stRest.nextToken())
+//                );
+//                otherRest.add(otherLineItem);
+//            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -63,6 +95,24 @@ public class InputAsPojo {
         Object thing4;
 
         public LineItem(Object thing1, Object thing2, Object thing3, Object thing4) {
+            this.thing1 = thing1;
+            this.thing2 = thing2;
+            this.thing3 = thing3;
+            this.thing4 = thing4;
+        }
+    }
+
+    /**
+     * Also represents one line of input data.
+     */
+    public static class OtherLineItem {
+        // TODO refactor and rename these and give them more specific classes
+        Object thing1;
+        Object thing2;
+        Object thing3;
+        Object thing4;
+
+        public OtherLineItem(Object thing1, Object thing2, Object thing3, Object thing4) {
             this.thing1 = thing1;
             this.thing2 = thing2;
             this.thing3 = thing3;
